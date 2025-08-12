dfd46aa41ac677d4cfa4df3291290b13c0b759af
Ruan de Albuquerque santos <ruanalbuquerque@gmail.com>
2025-08-12 11:04:24 -0300

    adição da classe Decimais, correção da função 'descount_boletos', adição de elemento que garante que algum elemento tenha sido carregado e correção da função responsável por atualizar a sessão

--- brasil.py
+++ brasil.py
@@ -1,11 +1,9 @@
-from threading import Thread
+import re
 from datetime import datetime
 from typing import Union, Mapping
 from time import sleep
 
-from playwright.sync_api import sync_playwright, Frame, Request
-
-from decimais import Decimais
+from playwright.sync_api import sync_playwright, Request
 
 class Brasil:
     def __init__(self, user: str, password: str):
@@ -13,33 +11,16 @@ class Brasil:
         self.p_context = sync_playwright()
         self._user = user
         self._password = password
-        self._timer_observer = None
     
     def _refresh_session(self, request: Request):
-        print('requeste')
-        if request.url.endswith('modal-renovacao-sessao/modal-renovacao-sessao.html'):
-            self._page.locator('.modal-renovacao-sessao-botao-primario').click()
-        # modal-renovacao-sessao-botao modal-renovacao-sessao-botao-primario
+        if 'MODAL_RENOVACAO_SESSAO' in request.url:
+            try:
+                self._page.locator('.modal-renovacao-sessao-botao-primario').click()
+            except:
+                pass
         pass
-    
-    # def _refresh_session_two(self):
-    #     while True:
-    #         try:
-    #             self._page.locator('.modal-renovacao-sessao-botao-primario').click()
-    #             print('renovado')
-    #         except:
-    #             sleep(1)
         
-    # def verify_timer(self):
-    #     if not self.logged: return
-    #     timer_text = self._page.locator('#token-time').inner_text()
-    #     minutes = int(timer_text.split(' ')[1].split(':')[0])
-        
-    #     if minutes <= 5:
-    #         self.close()
-    #         self.login()
-    
-    def __enter__(self, *args):
+    def __enter__(self):
         self.login()
         return self
     
@@ -51,9 +32,6 @@ class Brasil:
         self._playwright = self.p_context.start()
         self._browser = self._playwright.chromium.launch(headless=False)
         self._page = self._browser.new_page()
-        # if not self._timer_observer:
-        #     self._timer_observer = Thread(target=self._refresh_session_two, name='timer observer', daemon=True)
-        #     self._timer_observer.start()
         
         # Cria um Listener que verifica quando renovar a sessão
         self._page.on("request", self._refresh_session)
@@ -89,36 +67,39 @@ class Brasil:
         endpoint = self.url_base + '~2Fcobranca~2Fconsultas.bb'
         
         self._page.goto(endpoint)
-        
+            
         # Garante que a página será carregada
         self._page.wait_for_selector(Elemento.id_iframe)
         frame = self._page.frame_locator(Elemento.id_iframe)
         
         frame.locator('select[name="tipoConsulta"]').select_option('HC07')
         
-        frame.locator('#dependenciaOrigem').fill('16047')
-        frame.locator('#numeroContratoOrigem').fill('110000')
         
-        # preechendo nosso número e valor do título
-        frame.locator('input[name="nossoNumero"]').fill(key)
-        
-        # Botão de confirmar
-        frame.locator(Elemento.id_btn_ok).click()
-        
+        agenc_inp = frame.locator('#dependenciaOrigem')
         btn_error = frame.locator(Elemento.id_btn_erro)
         content_page = frame.locator('.tabelaTrsResposta tbody tr')
         
-        while btn_error.count() == 0 and content_page.count() == 0:
-            sleep(0.1)
-        
-        if btn_error.count() > 0:
-            btn_error.click()
-            print('Erro ao tentar utilizar o banco')
-            return
+        while True:
+            while btn_error.count() == 0 and content_page.count() == 0 and agenc_inp.count() == 0:
+                sleep(0.1)
+            
+            if agenc_inp.count() > 0:
+                agenc_inp.fill('16047')
+                frame.locator('#numeroContratoOrigem').fill('110000')
+                # preechendo nosso número e valor do título
+                frame.locator('input[name="nossoNumero"]').fill(key)
+                # Botão de confirmar
+                frame.locator(Elemento.id_btn_ok).click()
+            elif btn_error.count() > 0:
+                btn_error.click()
+                print('Erro ao tentar utilizar o banco')
+                return
+            elif content_page.count() > 5 and agenc_inp.count() == 0:
+                break
         
         dados = {}
         
-        content_page = frame.locator('.tabelaTrsResposta tbody tr').all_inner_texts()
+        content_page = content_page.all_inner_texts()
         
         dados = dict(
             codigo = content_page[1].split('\t')[1].lower(),
@@ -156,29 +137,30 @@ class Brasil:
         self._page.wait_for_selector(Elemento.id_iframe)
         frame = self._page.frame_locator(Elemento.id_iframe)
         
-        frame.locator('#dependenciaOrigem').fill('16047')
-        frame.locator('#numeroContratoOrigem').fill('110000')
-        
-        # preechendo nosso número e valor do título
-        frame.locator('input[name="nossoNumero"]').press_sequentially(key)
-        frame.locator('input[name="valorTitulo"]').press_sequentially(value)
-        
-        # Botão de confirmar
-        frame.locator(Elemento.id_btn_ok).click()
-        
+        agenc_inp = frame.locator('#dependenciaOrigem')
         btn_nova = frame.locator(Elemento.id_btn_nova)
         btn_voltar = frame.locator(Elemento.id_btn_erro)
         
         while True:
-            if btn_nova.count() > 0:
+            while btn_nova.count() == 0 and btn_voltar.count() == 0 and agenc_inp.count() == 0:
+                sleep(0.1)
+
+            if agenc_inp.count() > 0:
+                agenc_inp.fill('16047')
+                frame.locator('#numeroContratoOrigem').fill('110000')
+                
+                frame.locator('input[name="nossoNumero"]').fill(key)
+                frame.locator('input[name="valorTitulo"]').fill(value)
+
+                frame.locator(Elemento.id_btn_ok).click()
+            elif btn_nova.count() > 0:
                 btn_nova.click()
                 return True
             elif btn_voltar.count() > 0:
                 btn_voltar.click()
                 return False
-            sleep(1)
 
-    def decount_boleto(self, key: str, value: Union[float, int, str], disc_value: Union[float, int, str]):
+    def descount_boleto(self, key: str, value: Union[float, int, str], disc_value: Union[float, int, str]) -> bool:
         data = self.consult_boleto(key, False)
         
         if isinstance(value, (int, float)): value = Decimais(value).text
@@ -197,17 +179,32 @@ class Brasil:
             reset = True
             
         if reset:
-            # self._log(chave=chave, valor_boleto=valor_boleto, valor_credito=valor_credito, operacao="Abatimento", retorno=mensagem_retorno)
             frame.locator(Elemento.id_btn_nova).click()
-            return
+            frame.locator('select[name="tipoConsulta"]').select_option('HC07')
+            return False
         
-        frame.locator('#span1').click()
-        frame.locator('#tr41 td a').click()
+        key_span = frame.locator('#span1')
+        while key_span.count() > 0:
+            key_span.click()
+            frame.locator('#tr41 td a').click()
         
-        frame.locator('#valorAbatimento').fill('15,15')
-        frame.locator(Elemento.id_btn_ok).click()
-        frame.locator(Elemento.id_btn_nova).click()
-        pass
+        abat_inp = frame.locator('#valorAbatimento')
+        btn_new = frame.locator(Elemento.id_btn_nova)
+        while True:
+            while abat_inp.count() == 0 and btn_new.count() == 0:
+                sleep(0.1)
+            
+            if btn_new.count() > 0:
+                btn_new.click()
+                break
+            elif abat_inp.count() > 0:
+                frame.locator('#valorTitulo').fill(value)
+                abat_inp.fill(disc_value)
+                frame.locator(Elemento.id_btn_ok).click()
+                
+            
+        frame.locator('select[name="tipoConsulta"]').select_option('HC07')
+        return True
         
     def close(self, raise_exception: Union[str, None] = None):
         if not self.logged:
@@ -224,13 +221,62 @@ class Brasil:
         
         if raise_exception:
             raise Exception(raise_exception)
-    
+
 class Elemento:
     id_iframe = '#idIframeAreaTransacional'
     id_btn_ok = 'input[name="botao.acao.ok"]'
     id_btn_nova = 'input[name="botao.acao.nova"]'
     id_btn_erro = 'input[name="botao.acao.retornaErro"]'
-# https://autoatendimento.bb.com.br/apf-apj-autoatendimento/src/shared/directives/modal-renovacao-sessao/modal-renovacao-sessao.html
-# https://eni.bb.com.br/eni1/matomo.php?e_c=MODAL_RENOVACAO_SESSAO&e_a=SISTEMA&e_n=MODAL_RENOVACAO_SESSAO%7C%7CEXIBIR&idsite=23&rec=1&r=487075&h=15&m=24&s=23&url=https%3A%2F%2Fautoatendimento.bb.com.br%2FWEB-INF%2Fjsp%2Fcobranca%2FHI05%2FHI05e.jsp&urlref=https%3A%2F%2Fautoatendimento.bb.com.br%2Fapf-apj-autoatendimento%2Findex.html%3Fv%3D3.9.4&uid=481958799&_id=02cf48e6edf5d378&_idts=1754417412&_idvc=1&_idn=0&_refts=0&_viewts=1754417412&send_image=1&pdf=1&qt=0&realp=0&wma=0&dir=0&fla=0&java=0&gears=0&ag=0&cookie=1&res=1280x720&_cvar=%7B%221%22%3A%5B%22IdServidorCliente%22%2C%22pxl1apj2b817_apj-aapj-4%22%5D%7D&gt_ms=132&pv_id=FcCRco
-# https://gru-col.eum-appdynamics.com/eumcollector/beacons/browser/v2/GR-AAB-CCB/adrum
-# modal-renovacao-sessao-botao modal-renovacao-sessao-botao-primario
+
+class Decimais:
+    def __init__(self, value_in_real: int | float | str):
+        if isinstance(value_in_real, str):
+            if "," in value_in_real:
+                value_in_real = value_in_real.replace(".", "").replace(",", ".")
+            if "-" in value_in_real:
+                value_in_real = "-" + value_in_real.replace("-", "")
+            value_in_real = float(value_in_real)
+            
+        self._real = value_in_real
+        self._cents = self._real_to_cent(value_in_real)
+    
+    def _real_to_cent(self, number: float | int) -> int:
+        return int(f"{number:.2f}".replace(".", ""))
+    
+    def _cent_to_real(self, number: int) -> float:
+        return number/100
+    
+    def __add__(self, other):
+        if isinstance(other, Decimais):
+            return Decimais(self._cent_to_real(self._cents + other._cents))
+        return NotImplemented
+    
+    def __sub__(self, other):
+        if isinstance(other, Decimais):
+            return Decimais(self._cent_to_real(self._cents - other._cents))
+        return NotImplemented
+    
+    def __mul__(self, other):
+        if isinstance(other, Decimais):
+            return Decimais(self._real * other._real)
+        return NotImplemented
+    
+    def __truediv__(self, other):
+        if isinstance(other, Decimais):
+            return Decimais(f"{self._real / other._real:.2f}")
+        return NotImplemented
+    
+    @property
+    def real(self) -> float:
+        return self._real
+    
+    @property
+    def cent(self) -> int:
+        return self._cents
+    
+    @property
+    def text(self) -> str:
+        return f"{self._real:,.2f}".replace(".", "___").replace(",", ".").replace("___", ",")
+    
+    def __repr__(self):
+        return f"{self._real:,.2f}".replace(".", "___").replace(",", ".").replace("___", ",")
